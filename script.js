dcc351b9540cbdb214ac6676796e69f863ce0b42
tanavishali <tanawishalrai5271@gmail.com>
2024-06-29 22:06:31 +0500

    add game logic

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-// console.log(document.querySelector(".message").textContent);
-"use strict";
+// document.querySelector(".message").textContent = "what is my number";
+// document.querySelector(".guess").value = 19;
+// document.querySelector(".guess").value;
+document.querySelector(".check").addEventListener("click", function () {
+  let input_number = Number(document.querySelector(".guess").value);
+  console.log(typeof input_number, input_number);
+  if (input_number === 0) {
+    document.querySelector(".message").textContent = "no number";
+  } else {
+    document.querySelector(".message").textContent = "number entered";
+  }
+  //   document.querySelector(".message").textContent = "correct number";
 
-document.querySelector(".message").textContent = "guess my number";
-// console.log(document.querySelector(".message").textContent);
-
-document.querySelector(".number").textContent = "?";
-document.querySelector(".score-number").textContent = 10;
-document.querySelector(".highscore-class").textContent = 18;
-// console.log(document.querySelector(".guess").value); // Log the value of the input element
-
-// document.querySelector(".guess").value = 10; // Set the value of the input element to 10
-
-document.querySelector(".guess").addEventListener("click", function () {
-  let value1 = Number(document.querySelector(".guess").value);
-  console.log(typeof value1, value1);
-  document.querySelector(".number").textContent = value1;
+  //   console.log("hello");
 });

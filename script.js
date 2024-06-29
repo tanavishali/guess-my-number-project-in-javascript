bda9f82a668a942c817708d82e0f8a0e4721c6d3
tanavishali <tanawishalrai5271@gmail.com>
2024-06-29 11:07:50 +0500

    add style

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ document.querySelector(".number").textContent = "?";
 document.querySelector(".score-number").textContent = 10;
 document.querySelector(".highscore-class").textContent = 18;
 // console.log(document.querySelector(".guess").value); // Log the value of the input element
+
 // document.querySelector(".guess").value = 10; // Set the value of the input element to 10
+
 document.querySelector(".guess").addEventListener("click", function () {
   let value1 = Number(document.querySelector(".guess").value);
   console.log(typeof value1, value1);
